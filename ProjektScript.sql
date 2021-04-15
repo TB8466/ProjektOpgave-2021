a13fefafa108a83bc364dc5a6efbdcb993646c8d
TB8466 <78532614+TB8466@users.noreply.github.com>
2021-04-15 13:17:29 +0200

    Add files via upload

---
+++ ProjektScript.sql
@@ -0,0 +1,24 @@
+-- delete database and create new one
+DROP DATABASE IF EXISTS wishlist;
+CREATE DATABASE wishlist;
+
+USE wishlist;
+
+DROP TABLE IF EXISTS Users;
+CREATE TABLE Users (
+id INT NOT NULL AUTO_INCREMENT,
+username VARCHAR(45) NOT NULL,
+password VARCHAR(45) NOT NULL,
+PRIMARY KEY (id)
+);
+
+DROP TABLE IF EXISTS wishes;
+CREATE TABLE wishes (
+id INT NOT NULL AUTO_INCREMENT,
+name VARCHAR(45) NOT NULL,
+price VARCHAR(45) NOT NULL,
+url VARCHAR(300),
+user_id INT NOT NULL,
+PRIMARY KEY (id),
+FOREIGN KEY (user_id) REFERENCES users(id)
+);
